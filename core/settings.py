c1171041e72d39b4ec4e264b173949c75af8d1f0
Yago Ferreira <34385672+YagoFGomes@users.noreply.github.com>
2023-11-23 20:53:51 +0000

    Update settings.py

--- core/settings.py
+++ core/settings.py
@@ -119,7 +119,7 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 STATICFILES_DIRS = [BASE_DIR / 'staticfiles',]
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = os.path.join(BASE_DIR, 'root_staticfiles')
 
 
 # Default primary key field type
